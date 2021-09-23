86b1dc17ca5776ee23f4e06fda347cdc891e160f
Zuhair Abbas <zuhairabs@gmail.com>
2021-09-23 17:11:47 +0530

    Cors added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require('path');
 const app = express();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'))
@@ -9,6 +10,13 @@ app.use(express.json())
 const connectDB = require('./config/db');
 connectDB();
 
+// Cors
+//app.use(cors)
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+app.use(cors(corsOptions));
 //Template Engine
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');

8c6983da8402a61b7beb83e3d94f4e133292779f
Zuhair Abbas <zuhairabs@gmail.com>
2021-09-23 18:16:15 +0530

    Added script.js | Home Url

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 // Routes
+app.get("/", (req, res) => {
+  res.render('home');
+})
 app.use("/api/files", require('./routes/files'));
 app.use("/files", require('./routes/show'));
 app.use("/files/download", require('./routes/download'));
